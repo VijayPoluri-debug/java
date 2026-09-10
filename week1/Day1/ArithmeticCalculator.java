a515218e26859c4d7be8fbaea66a179b9dcddaf4
VijayPoluri-debug <vijaypoluri857@gmail.com>
2026-09-09 23:03:13 -0400

    first commit

---
+++ week1/Day1/ArithmeticCalculator.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ArithmeticCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        double addition = num1 + num2;
+        double subtraction = num1 - num2;
+        double multiplication = num1 * num2;
+
+        System.out.println("Addition: " + addition);
+        System.out.println("Subtraction: " + subtraction);
+        System.out.println("Multiplication: " + multiplication);
+
+        if (num2 != 0) {
+            double division = num1 / num2;
+            System.out.println("Division: " + division);
+        } else {
+            System.out.println("Division: Cannot divide by zero");
+        }
+
+        scanner.close();
+    }
+}
---
+++ week1/Day1/StudentDetails.java
@@ -0,0 +1,18 @@
+public class StudentDetails {
+
+    public static void main(String[] args) {
+
+        int age = 24;
+        double gpa = 3.8;
+        char grade = 'A';
+        boolean isEnrolled = true;
+        String name = "Vijay";
+
+        System.out.println("Student Details");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("GPA: " + gpa);
+        System.out.println("Grade: " + grade);
+        System.out.println("Enrolled: " + isEnrolled);
+    }
+}
